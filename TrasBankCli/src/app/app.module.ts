3a6ec8b6e1f06ff104e8b0ce377770c877168ec0
Sergibelgo <sergibelgo1@gmail.com>
2023-05-17 20:41:29 +0200

    Comienzo pagina de inicio

--- TrasBankCli/src/app/app.module.ts
+++ TrasBankCli/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { IndexComponent } from './Components/index/index.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ScrollTopComponent } from './Components/scroll-top/scroll-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    FooterComponent,
+    ScrollTopComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
